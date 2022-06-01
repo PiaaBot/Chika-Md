39cc609259c1599ca435693d20638d0c310c59ce
PiaaBot <106659347+PiaaBot@users.noreply.github.com>
2022-06-01 15:57:20 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,11 +22,11 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['628972462200','628972462200','628972462200'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['628972462200'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = '628972462200' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
-//—————「 Set Nama Own & Bot 」—————//
+//—————「 Set Piaa & Piaa-Bot 」—————//
 global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
 global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
@@ -41,15 +41,15 @@ global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Piaa-Bot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Pia' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
 global.sessionName = 'session'
 
 //—————「 Set Limit 」—————//
-//terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
+//30, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
 free: 15,
